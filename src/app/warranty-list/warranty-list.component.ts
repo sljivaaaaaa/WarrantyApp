77e9c3465341087abb69e5fbacfb19fb1c027e1c
Ivana Sedmakova <ivana.sedmakova@damilah.com>
2024-05-29 13:08:41 +0200

    Updated the table to use Angular Material

--- src/app/warranty-list/warranty-list.component.ts
+++ src/app/warranty-list/warranty-list.component.ts
@@ -1,33 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table'; 
 import { Warranty } from '../warranty.model';  
 import { WarrantyService } from '../warranty.service';
 
 @Component({
   selector: 'app-warranty-list',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatTableModule], 
   templateUrl: './warranty-list.component.html',
-  styleUrls: ['./warranty-list.component.css'],
-  imports: [CommonModule, FormsModule],  // Add FormsModule here
-  standalone: true
+  styleUrls: ['./warranty-list.component.css']
 })
 export class WarrantyListComponent implements OnInit {
-  warranties: Warranty[] = [];
-  filteredWarranties: Warranty[] = [];
+  displayedColumns: string[] = ['fullName', 'serialNumber', 'manufacturer', 'model', 'purchaseDate', 'warrantyInYears', 'extraCare'];
+  dataSource: Warranty[] = []; 
   searchTerm: string = '';
 
   constructor(private warrantyService: WarrantyService) { }
 
   ngOnInit(): void {
-    this.warranties = this.warrantyService.getWarranties();
-    this.filteredWarranties = this.warranties;
+    this.loadWarranties();
+  }
+
+  loadWarranties(): void {
+    this.dataSource = this.warrantyService.getWarranties();
   }
 
   search(): void {
     if (!this.searchTerm) {
-      this.filteredWarranties = this.warranties;
+      this.loadWarranties();
     } else {
-      this.filteredWarranties = this.warranties.filter(warranty =>
+      this.dataSource = this.dataSource.filter((warranty: Warranty) => 
         warranty.fullName.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
         warranty.serialNumber.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
         warranty.manufacturer.toLowerCase().includes(this.searchTerm.toLowerCase()) ||
